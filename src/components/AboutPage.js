9309223cd31523d9c398cd4765a420c0c770799a
hxg220033 <hxg220033@utdallas.edu>
2023-04-26 19:59:55 -0500

    change about Page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,9 +5,25 @@ function AboutPage() {
     return (
         <div className="container">
             <div style={{padding: '25px', fontSize: '20px',}}>
-                <h2 style={{paddingBottom: '125px'}}>Value Proposition: </h2>
-                <h2 style={{paddingBottom: '125px'}}>Features: </h2>
-                <h2 style={{paddingBottom: '125px'}}>Missions and Values: </h2>
+                <h2 >Value Proposition: </h2>
+                <p>
+                Unique – First emotion tester for human by machine.<br/>
+                Flexible – Easy to use and understand, unlimited.<br/>
+                Diverse – For all human-being, all language, all group.<br/>
+                </p>
+                <h2 >Features: </h2>
+                <p>
+                Intelligent – Thinking like human, get the emotion from human by their words, speed and tone.<br/>
+                Emotion Learning Machine – The beginning of machine learning emotion from human.<br/>
+                Adaptive Learning – Emotion-GPT will learn by input and get more and more data by users.<br/>
+
+                </p>
+                <h2 >Missions and Values: </h2>
+                <p>
+                Criminology and Psychology – Get the emotion from the criminal or the patients.<br/>
+                Disability – Better understand the emotion of people who has language disability.<br/>
+                Future – Help Artificial Intelligent learning emotion from human and may be set up the emotion for AI.<br/>
+                </p>
                 <h2>Team Members: </h2>
             </div>
         </div>
